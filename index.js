2caeaa7b8acc2d8aa9fca97be043dc9d96188d6e
gabrielmz719 <g20998986@gmail.com>
2025-02-22 07:30:12 -0300

    Criei a função playGame para jogar uma partida de pedra papel tesoura

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-function getComputerChoice(){
-    var escolha = ['pedra','papel','tesoura'];
-    var sorteioPalavra = escolha[Math.floor(Math.random() * escolha.length )] ;
-    return sorteioPalavra ;
+function getComputerChoice() {
+    var escolha = ['pedra', 'papel', 'tesoura'];
+    var sorteioPalavra = escolha[Math.floor(Math.random() * escolha.length)];
+    return sorteioPalavra;
 }
-console.log(getComputerChoice())
 
-function getHumanChoice(){
-    let escolhaHumano =  prompt('Escolha entre pedra, papel ou tesoura!')
+function getHumanChoice() {
+    let escolhaHumano = prompt('Escolha entre pedra, papel ou tesoura!')
     return escolhaHumano.toLocaleLowerCase();
 }
-console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLocaleLowerCase();
+    computerChoice.toLocaleLowerCase();
+
+    if (computerChoice === 'tesoura' && humanChoice === 'pedra' || computerChoice === 'papel' && humanChoice === 'tesoura' || computerChoice === 'pedra' && humanChoice === 'papel') {
+        console.log(`Voce escolheu ${humanChoice} o computador escolheu ${computerChoice}, voce venceu`);
+        humanScore + 1;
+    } else if (computerChoice === humanChoice) {
+
+        console.log(`Voce escolheu ${humanChoice} o computador escolheu ${computerChoice}, empate`);
+
+    } else if (computerChoice === 'tesoura' && humanChoice === 'papel' || computerChoice === 'papel' && humanChoice === 'pedra' || computerChoice === 'pedra' && humanChoice === 'tesoura') {
+        console.log(`Voce escolheu ${humanChoice} o computador escolheu ${computerChoice}, voce perdeu`);
+        computerScore + 1;
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playGame() {
+    playRound(humanSelection, computerSelection);
+}
+
+playGame();
